c9e7a93f25b8a0f8ded54cf5ec96823ecddcb06a
Sheikh Musalin <sheikhmusalin@gmail.com>
2024-02-06 14:08:20 -0500

    Authentication

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,10 @@
-import {Button} from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs"
 
 export default function DashboardPage() {
   return (
-    <Button variant="destructive">Click Me</Button>
-  )
+    <div>
+    <p>Dashboard Page (Protected)</p>
+    <UserButton afterSignOutUrl="/"/>
+    </div>
+    )
 }
